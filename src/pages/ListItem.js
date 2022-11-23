67a7aa1a7d8490d7b5306ff53faa566580b49dd0
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-11-23 11:00:47 +0100

    Create a component that list each stock

---
+++ src/pages/ListItem.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ListItem = ({ stock }) => (
+  <li>
+    <Link to={`/details/${stock.symbol}`}>
+      <div>
+        <p>
+          <span>Name:</span>
+          {' '}
+          {stock.companyName}
+        </p>
+        <p>
+          <span>Industry:</span>
+          {' '}
+          {stock.industry}
+        </p>
+        <p>
+          <span>Price per Stock:</span>
+          {' '}
+          $
+          {stock.price}
+        </p>
+        <p>
+          <span>Market Cap:</span>
+          {' '}
+          $
+          {stock.marketCap.toLocaleString()}
+        </p>
+        <p>
+          <span>
+            See about
+            {' '}
+            {stock.symbol}
+          </span>
+          {' '}
+          <i className="fa-sharp fa-solid fa-arrow-right" />
+        </p>
+      </div>
+    </Link>
+  </li>
+);
+
+ListItem.propTypes = {
+  stock: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default ListItem;
--- src/redux/APIs.js
+++ src/redux/APIs.js
@@ -1,6 +1,6 @@
 const apiKey = '76a5197d8943e2e227cf03310c1ab4b7';
 
-const getAllStocks = `https://financialmodelingprep.com/api/v3/stock-screener?limit=5000&apikey=${apiKey}`;
+const getAllStocks = `https://financialmodelingprep.com/api/v3/stock-screener?limit=3642&apikey=${apiKey}`;
 
 const getStockProfile = (stock) => `https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=${apiKey}`;
 
